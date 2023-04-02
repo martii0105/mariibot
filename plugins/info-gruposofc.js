0ea05abbe8d0a0b93a8e36478a22a168df3b6b43
Natixemer <129608393+Natixemer@users.noreply.github.com>
2023-04-01 21:18:50 -0500

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,7 +1,7 @@
 
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `
-Hola 👋🏻, Bienvenido a los grupos oficiales, te invito a unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de The-LoliBot-MD 😸
+Hola 👋🏻, Bienvenido a los grupos oficiales, te invito a unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de 𝐍𝐚𝐭𝐢𝐱𝐂𝐡𝐚𝐫𝐥𝐢𝐞-MD 😸
 
 ➤ Grupos oficiales del bot:
 1) *${nn}*
@@ -23,20 +23,20 @@ Hola 👋🏻, Bienvenido a los grupos oficiales, te invito a unete a los grupos
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 Grupos del amistades donde esta el bot
 
-*꧁⁣𓆩Aᴹᴵᴳᴼˢ Cᴴᴬᵀˢ Y Aᴾᴼᴿᵀᴱ*
-https://chat.whatsapp.com/JZaD3sfNoVW4JvaoQ4uVwF
+*Grupo de Spam 𝐍𝐚𝐭𝐢𝐱𝐂𝐡𝐚𝐫𝐥𝐢𝐞*
+https://chat.whatsapp.com/J3V9AXDtnY81vT9lENf7dq
 
 *⇶⃤꙰𝑬𝒏𝒍𝒂𝒄𝒆 𝒍𝒐𝒍𝒊𝒃𝒐𝒕ꦿ⃟⃢*
 *${nnnttt}*
 
-*꧁⃢⃟҉💫🌺ঔঔ𝔼ℕ𝕃𝔸ℂ𝔼𝕊 𝕃𝔸 𝕁𝔼𝔽𝔸✨*
-https://chat.whatsapp.com/JegOp8NUSMd0vn75s4hkaj
+*Grupo Pruebas Bot*
+https://chat.whatsapp.com/C8UfYBHRzSqIvZpMrbPc2c
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 
 Si quieres que tu grupo aparezca aca habla con mi creador
 `.trim() 
 
-conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '𝙏𝙝𝙚-𝙇𝙤𝙡𝙞𝘽𝙤𝙩-𝙈𝘿', 'status@broadcast')
+conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '𝐍𝐚𝐭𝐢𝐱𝐂𝐡𝐚𝐫𝐥𝐢𝐞-𝙈𝘿', 'status@broadcast')
 }
 handler.command = /^linkgc|grupos|gruposgatabot|gatabotgrupos|gruposdegatabot|groupofc|gruposgb|grupogb|groupgb$/i
 
