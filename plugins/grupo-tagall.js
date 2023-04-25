51321cb05983efd5c5a7289430c19ea557ab2e92
Emergit2006 <127906293+Emergit2006@users.noreply.github.com>
2023-04-25 04:25:24 -0500

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -5,11 +5,11 @@ throw false
 }
 let vn = './media/Invocar.mp3'
 let pesan = args.join` `
-let oi = `*Bot De http://wa.me/+51902214891* ${pesan}`
-let teks = `〔 *${wm}* 〕\n 𝐃𝐞𝐤𝐨 𝐥𝐨𝐬 𝐝𝐞𝐬𝐩𝐢𝐞𝐫𝐭𝐚 𝐦𝐨𝐜𝐡𝐨𝐬🍄 \n\n ${oi}\n\n *𝙀𝙩𝙞𝙦𝙪𝙚𝙩𝙖𝙨:*\n`
+let oi = `*Bot Creado por CharliexNati* ${pesan}`
+let teks = `〔 *${wm}* 〕\n 𝐂𝐡𝐚𝐫𝐥𝐢𝐞𝐱𝐍𝐚𝐭𝐢 𝐥𝐨𝐬 𝐝𝐞𝐬𝐩𝐢𝐞𝐫𝐭𝐚 𝐦𝐨𝐜𝐡𝐨𝐬 \n\n ${oi}\n\n *𝙀𝙩𝙞𝙦𝙪𝙚𝙩𝙖𝙨:*\n`
 for (let mem of participants) {
-teks += `🍓 @${mem.id.split('@')[0]}\n`}
-teks += `└ @deko.pe\n\n*<3*`
+teks += `🌱 @${mem.id.split('@')[0]}\n`}
+teks += `└ @emx._xit\n\n*<3*`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 conn.sendFile(m.chat, vn, 'Invocar.mp3', null, m, true, { type: 'audioMessage', ptt: true, sendEphemeral: true })
 }
