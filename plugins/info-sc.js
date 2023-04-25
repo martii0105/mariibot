dfb6768ae44211442667894dfb8cad25ace68903
Emergit2006 <127906293+Emergit2006@users.noreply.github.com>
2023-04-25 04:26:24 -0500

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -3,7 +3,7 @@ handler.all = async function (m) {
 let chat = global.db.data.chats[m.chat]
 
 if (/^sc|github$/i.test(m.text) ) { //sem prefixo
-    conn.reply(m.chat, `_https://github.com/elrebelde21/The-LoliBot-MD_`, m) //wm, null, [['Menu', '#menu']], m) botones :V
+    conn.reply(m.chat, `_https://github.com/Emergit2006/Charlie-MD-V4_`, m) //wm, null, [['Menu', '#menu']], m) botones :V
 
 }
 
