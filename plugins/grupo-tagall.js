88b83c41d4f19c40be5d15585cdba9c4503fbf4c
martii0105 <128637734+martii0105@users.noreply.github.com>
2023-06-03 04:29:38 +0200

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -6,16 +6,16 @@ throw false
 let vn = './media/Invocar.mp3'
 let pesan = args.join` `
 let oi = `*Bot Creado por CharliexNati* ${pesan}`
-let teks = `〔 *${wm}* 〕\n 𝐂𝐡𝐚𝐫𝐥𝐢𝐞𝐱𝐍𝐚𝐭𝐢 𝐥𝐨𝐬 𝐝𝐞𝐬𝐩𝐢𝐞𝐫𝐭𝐚 𝐦𝐨𝐜𝐡𝐨𝐬 \n\n ${oi}\n\n *𝙀𝙩𝙞𝙦𝙪𝙚𝙩𝙖𝙨:*\n`
+let teks = `〔 *${wm}* 〕\n 《⺀𝑹𝒆𝒗𝒊𝒗𝒂𝒏 𝒑𝒍𝒂𝒏𝒕𝒂𝒔 - 𝒕𝒆𝒏𝒆𝒎𝒐𝒔 𝒗𝒆𝒓𝒔𝒖𝒔 💋⺀》 \n\n ${oi}\n\n 《 𝐄𝐭𝐢𝐪𝐮𝐞𝐭𝐚𝐬 》:\n`
 for (let mem of participants) {
-teks += `🌱 @${mem.id.split('@')[0]}\n`}
-teks += `└ @emx._xit\n\n*<3*`
+teks += `✼👸🏻 @${mem.id.split('@')[0]}\n`}
+teks += `➥ 𝐌𝐚𝐫𝐢𝐚 𝐁𝐨𝐭\n\n*<3*`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 conn.sendFile(m.chat, vn, 'Invocar.mp3', null, m, true, { type: 'audioMessage', ptt: true, sendEphemeral: true })
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
 handler.tags = ['group']
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
+handler.command = /^(sorras|putas|todos|todassorras)$/i
 handler.admin = true
 handler.group = true
 export default handler
